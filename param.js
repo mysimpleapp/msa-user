c4a2fc8ed4ff6ea1b9352d8c086590a532976e67
Nicolas CARREZ <nicolas.carrez@clustree.com>
2020-02-12 14:22:50 +0100

    formmatting

--- param.js
+++ param.js
@@ -4,50 +4,50 @@ const { Perm, PermNum } = require("./perm")
 const { Param } = Msa.require("params")
 
 const ParamPerm = exp.ParamPerm = class extends Param {
-	getAsJsonable(kwargs){
+	getAsJsonable(kwargs) {
 		const perm = this.value
-		if(kwargs && kwargs.noDefaults)
+		if (kwargs && kwargs.noDefaults)
 			return perm.expr
 		else
 			return perm.getExpr()
 	}
-	setFromJsonable(val){
+	setFromJsonable(val) {
 		this.value.expr = val
 	}
-	getViewer(){
+	getViewer() {
 		return { wel: "/user/msa-user-perm-viewer.js" }
 	}
-	getEditor(){
+	getEditor() {
 		return { wel: "/user/msa-user-perm-editor.js" }
 	}
 }
 
-Perm.newParam = function(val) {
+Perm.newParam = function (val) {
 	const perm = new this(val)
 	return new ParamPerm(perm)
 }
 
-PermNum.newParam = function(val) {
+PermNum.newParam = function (val) {
 	const perm = new this(val)
 	const param = new ParamPerm(perm)
 	const labels = this.prototype.getLabels()
-	const labelsAttr = labels ? { labels:labels.map(l => l.name) } : null
-	
-	param.getViewer = function(){
+	const labelsAttr = labels ? { labels: labels.map(l => l.name) } : null
+
+	param.getViewer = function () {
 		return {
 			wel: "/user/msa-user-perm-viewer.js",
 			tag: "msa-user-perm-num-viewer",
 			attrs: labelsAttr
 		}
 	}
-	
-	param.getEditor = function(){
+
+	param.getEditor = function () {
 		return {
 			wel: "/user/msa-user-perm-editor.js",
 			tag: "msa-user-perm-num-editor",
 			attrs: labelsAttr
 		}
 	}
-	
+
 	return param
 }
